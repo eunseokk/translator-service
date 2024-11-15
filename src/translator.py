f08ccf1bfb90ac47bf4e90a44e193307d112bb72
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2024-11-15 13:14:05 -0500

    adding api key code

--- src/translator.py
+++ src/translator.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 client = AzureOpenAI(
-    api_key=os.getenv("API_KEY"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     api_version="2024-02-15-preview",
     azure_endpoint="https://bluesleep-ai.openai.azure.com/"
 )
